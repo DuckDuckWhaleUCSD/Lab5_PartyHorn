df4ce5f8cb6ff1c0c818bf2bf5443c4aeea9f491
DuckDuckWhaleUCSD <DuckDuckWhaleUCSD@noreply.github.com>
2021-02-05 20:25:30 -0800

    Add: radio button logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,10 @@ audio = document.getElementById('horn-sound');
 number = document.getElementById('volume-number');
 slider = document.getElementById('volume-slider');
 volume_image = document.getElementById('volume-image');
+sound_image = document.getElementById('sound-image');
+air_radio = document.getElementById('radio-air-horn');
+car_radio = document.getElementById('radio-car-horn');
+paty_radio = document.getElementById('radio-party-horn');
 
 // takes a string in the form of 0-100 as integer
 function update_volume(value) {
@@ -20,4 +24,19 @@ number.addEventListener('input', function() {
 
 slider.addEventListener('input', function() {
 	update_volume(slider.value);
+});
+
+air_radio.addEventListener('input', function() {
+	audio.src = 'assets/media/audio/air-horn.mp3';
+	sound_image.src = 'assets/media/images/air-horn.svg'
+});
+
+car_radio.addEventListener('input', function() {
+	audio.src = 'assets/media/audio/car-horn.mp3';
+	sound_image.src = 'assets/media/images/car.svg'
+});
+
+paty_radio.addEventListener('change', function() {
+	audio.src = 'assets/media/audio/party-horn.mp3';
+	sound_image.src = 'assets/media/images/party-horn.svg'
 });
