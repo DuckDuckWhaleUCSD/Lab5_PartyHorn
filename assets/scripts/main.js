e8a9d2a56ab14ea9b3c7239076e67c099a64d864
DuckDuckWhaleUCSD <DuckDuckWhaleUCSD@noreply.github.com>
2021-02-05 20:16:54 -0800

    Add: volume updating logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,23 @@
 // main.js
 
-// TODO
+audio = document.getElementById('horn-sound');
+number = document.getElementById('volume-number');
+slider = document.getElementById('volume-slider');
+volume_image = document.getElementById('volume-image');
+
+// takes a string in the form of 0-100 as integer
+function update_volume(value) {
+	num = Number(value);
+	audio.volume = num / 100;
+	number.value = value;
+	slider.value = value;
+	volume_image.src = "assets/media/icons/volume-level-" + Math.min(Math.ceil(num / 33).toString(), 3) + ".svg";
+}
+
+number.addEventListener('input', function() {
+	update_volume(number.value);
+});
+
+slider.addEventListener('input', function() {
+	update_volume(slider.value);
+});
